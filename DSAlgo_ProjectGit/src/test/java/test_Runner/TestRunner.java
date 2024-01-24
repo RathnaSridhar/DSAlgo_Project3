e7129c0d11f410374bdaca9eaca0da92ef17ed50
chand <chand@192.168.99.46>
2024-01-23 22:19:58 -0800

    BugSlayers DSAlgo_Project

---
+++ DSAlgo_ProjectGit/src/test/java/test_Runner/TestRunner.java
@@ -0,0 +1,17 @@
+package test_Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions (tags = "",
+				features = {"src/test/resources/DS_Feature"},
+				glue = {"stepdefinitions_DS", "hooks"},
+				plugin = {"pretty", "html:target/htmreport.html"})
+public class TestRunner extends AbstractTestNGCucumberTests{
+//	@Override
+//	@DataProvider(parallel = true)
+//	public Object[][] scenarios(){
+//		return super.scenarios();
+//	}
+//	
+}
